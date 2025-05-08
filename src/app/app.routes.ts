11da454348af91fb6b219a7ed07612fa2a3eba8a
Humberto Fernandes <humbertofernandes08@hotmail.com>
2025-05-08 17:34:11 -0300

    Adicionando configurações iniciais do projeto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomePublicaComponent } from './components/home-publica/home-publica.component';
+import { UsuarioCadastroComponent } from './components/usuario-cadastro/usuario-cadastro.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', pathMatch: 'full', redirectTo: 'public-home',
+    },
+    {
+        path: 'public-home', component: HomePublicaComponent,
+    },
+    {
+        path: 'cadastro', component: UsuarioCadastroComponent,
+    }
+];
