62dc08ea3acaa2e78a3916cbbdd326e2cc1127e9
viniciusprogr <vnsrodrigues10@gmail.com>
2025-05-12 19:15:19 -0300

    Refatorando estilo da aplicação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { heroUsers, heroHome, heroUserCircle, heroBookmark, heroCog8Tooth } from '@ng-icons/heroicons/outline';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  viewProviders: [provideIcons({heroUsers, heroHome, heroUserCircle, heroBookmark, heroCog8Tooth})]
 })
 export class AppComponent {
   title = 'NesterFront';
--- src/app/components/feed/feed.component.ts
+++ src/app/components/feed/feed.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { heroUsers } from '@ng-icons/heroicons/outline';
 
 @Component({
   selector: 'app-feed',
   standalone: true,
-  imports: [],
+  imports: [NgIcon],
   templateUrl: './feed.component.html',
-  styleUrl: './feed.component.css'
+  styleUrl: './feed.component.css',
+  viewProviders: [provideIcons({heroUsers})]
 })
 export class FeedComponent {
 
