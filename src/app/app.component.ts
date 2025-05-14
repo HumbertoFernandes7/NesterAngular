5ae6332825f7973179b0e4548f346c5b18c8d6de
viniciusprogr <vnsrodrigues10@gmail.com>
2025-05-14 02:53:03 -0300

    atualizando UX

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { provideIcons } from '@ng-icons/core';
-import { heroUsers, heroHome, heroUserCircle, heroBookmark, heroCog8Tooth, heroPlusCircle, heroXMark } from '@ng-icons/heroicons/outline';
+import { heroUsers, heroHome, heroUserCircle, heroBookmark, heroCog8Tooth, heroPlusCircle, heroXMark, heroHeart, heroChatBubbleOvalLeft } from '@ng-icons/heroicons/outline';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +10,7 @@ import { heroUsers, heroHome, heroUserCircle, heroBookmark, heroCog8Tooth, heroP
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  viewProviders: [provideIcons({heroUsers, heroHome, heroUserCircle, heroBookmark, heroCog8Tooth, heroPlusCircle, heroXMark})]
+  viewProviders: [provideIcons({heroUsers, heroHome, heroUserCircle, heroBookmark, heroCog8Tooth, heroPlusCircle, heroXMark, heroHeart, heroChatBubbleOvalLeft})]
 })
 export class AppComponent {
   title = 'NesterFront';
